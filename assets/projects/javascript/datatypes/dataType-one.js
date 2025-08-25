7dedbf98c8a1e4d86f731df1930583f1709a30db
digrasekhushal <158585751+digrasekhushal@users.noreply.github.com>
2025-08-25 15:41:42 +0530

    Add files via upload

---
+++ assets/projects/javascript/datatypes/dataType-one.js
@@ -0,0 +1,38 @@
+ // number datatype ----------
+      let num = 20;
+      console.log(num);
+      // string datatype ----------
+      let str = "Hey, I am leaning javaScript!";
+      console.log(str);
+
+      // boolean datatype ----------
+      let bool = true;
+      console.log(bool);
+
+      // character datatype ----------
+      let char = "A";
+      console.log(char);
+
+      // float datatype ----------
+      let float = 20.5;
+      console.log(float);
+
+      // array datatype ----------
+      let arr = [1, 2, 3, 4, 5];
+      console.log(arr);
+
+      // Dictionary datatype ----------
+      let dict = { name: "Khushal", age: 19 };
+      console.log(dict);
+
+      // object datatype ----------
+      let obj = {
+        name: "Khushal D",
+        age: 19,
+        city: "Katol",
+      };
+      console.log(obj);
+
+      // null datatype ----------
+      let nullVar = null;
+      console.log(nullVar);
